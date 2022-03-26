4c0a6771bef3ad48f378741ee2c723e0e427f333
johnnyvaz1 <johnny.ferreira@luizalabs.com>
2022-03-26 20:53:20 -0300

    feat: inicio commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+// #include <AFMotor.h>//Biblioteca do motor shield
+// #include <Ultrasonic.h>//Bibloteca do sensor ultrassônico
+// #include <LiquidCrystal_I2C.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
